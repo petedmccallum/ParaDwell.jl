070207a8ed710776b79451b1cb34641339f7e67a
petedmccallum <p.mccallum@hw.ac.uk>
2020-11-21 21:30:05 +0000

    Features partially out of bounds cropped

--- src/UI/ClickTile.jl
+++ src/UI/ClickTile.jl
@@ -1,20 +1,30 @@
 include("MainUI_Launch.jl")
 
-function LayerTrace(gml,target;colour::String="#aaaaaa",CropOutOfBounds::Bool=false)
+function LayerTrace(gml,target,tileRegister;colour::String="#aaaaaa",CropOutOfBounds::Bool=false)
     iKeep = findall([length(findall(descr.==target))>0 for descr in gml.summary.descr])
 
     xs = vcat(gml.summary.eastings[iKeep]...)
     ys = vcat(gml.summary.northings[iKeep]...)
 
     if CropOutOfBounds == true
-        ob_up = findall([sum(y.>tileRegister.northings_max[iTile])==length(y) for y in ys])
-        ob_down = findall([sum(y.<tileRegister.northings_min[iTile])==length(y) for y in ys])
-        ob_left = findall([sum(x.<tileRegister.eastings_min[iTile])==length(x) for x in xs])
-        ob_right = findall([sum(x.>tileRegister.eastings_max[iTile])==length(x) for x in xs])
+        ob_up = findall([sum(y.>tileRegister.northings_max)==length(y) for y in ys])
+        ob_down = findall([sum(y.<tileRegister.northings_min)==length(y) for y in ys])
+        ob_left = findall([sum(x.<tileRegister.eastings_min)==length(x) for x in xs])
+        ob_right = findall([sum(x.>tileRegister.eastings_max)==length(x) for x in xs])
 
         iOB = unique(vcat(ob_up,ob_down,ob_left,ob_right))
         xs = xs[Not(iOB)]
         ys = ys[Not(iOB)]
+
+        ob_up = [findall(y.>tileRegister.northings_max) for y in ys]
+        ob_down = [findall(y.<tileRegister.northings_min) for y in ys]
+        ob_left = [findall(x.<tileRegister.eastings_min) for x in xs]
+        ob_right = [findall(x.>tileRegister.eastings_max) for x in xs]
+
+        iRm = [unique(vcat(ob_up[i],ob_down[i],ob_left[i],ob_right[i])) for i in 1:length(xs)]
+        [xs[i] = xs[i][Not(iRm[i])] for i in 1:length(xs)]
+        [ys[i] = ys[i][Not(iRm[i])] for i in 1:length(xs)]
+
     end
 
     xs = vcat([vcat(pts,NaN) for pts in xs]...)
@@ -44,28 +54,28 @@ on(plt_nationwide["click"]) do dat
     # Clear blank initialising trace, optionally clear all traces
     iRm = findall([tr[:name] for tr in ui.p0.plot.data].=="blank")
     [deletetraces!(ui.p0,i) for i in iRm]
-    [deletetraces!(ui.p0,i) for i in length(ui.p0.plot.data):-1:1]
+    # [deletetraces!(ui.p0,i) for i in length(ui.p0.plot.data):-1:1]
 
     # Load data for new tile
     gml = ParaDwell.ProcessGMLs(env.paths[:OS],tileRegister.tileRefs[iTile])
 
     # Plot layers
     traces = []
-    # push!(traces,LayerTrace(gml,"General Surface",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Path",colour="#aaaaaa"))
-    push!(traces,LayerTrace(gml,"Building",colour="#0984e3"))
-    push!(traces,LayerTrace(gml,"Road Or Track",colour="#ff9f43",CropOutOfBounds=true))
-    # push!(traces,LayerTrace(gml,"Roadside",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Structure",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Landform",colour="#aaaaaa"))
-    push!(traces,LayerTrace(gml,"Natural Environment",colour="#aaaaaa",CropOutOfBounds=false))
-    # push!(traces,LayerTrace(gml,"Tidal Water",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Inland Water",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Unclassified",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Historic Interest",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Network Or Polygon Closing Geometry",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"General Feature",colour="#aaaaaa"))
-    # push!(traces,LayerTrace(gml,"Terrain And Height",colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"General Surface",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Path",tileRegister[iTile,:],colour="#aaaaaa"))
+    push!(traces,LayerTrace(gml,"Building",tileRegister[iTile,:],colour="#0984e3"))
+    push!(traces,LayerTrace(gml,"Road Or Track",tileRegister[iTile,:],colour="#ff9f43",CropOutOfBounds=true))
+    # push!(traces,LayerTrace(gml,"Roadside",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Structure",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Landform",tileRegister[iTile,:],colour="#aaaaaa"))
+    push!(traces,LayerTrace(gml,"Natural Environment",tileRegister[iTile,:],colour="#aaaaaa",CropOutOfBounds=true))
+    # push!(traces,LayerTrace(gml,"Tidal Water",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Inland Water",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Unclassified",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Historic Interest",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Network Or Polygon Closing Geometry",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"General Feature",tileRegister[iTile,:],colour="#aaaaaa"))
+    # push!(traces,LayerTrace(gml,"Terrain And Height",tileRegister[iTile,:],colour="#aaaaaa"))
 
     # Plot tile boundary
     push!(traces,scatter(
